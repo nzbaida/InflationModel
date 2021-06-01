6f9dcae19112d067a80f0d50e493f1ec4a7612b7
nzbaida <76817348+nzbaida@users.noreply.github.com>
2021-06-01 06:50:05 -0400

    Update Final.r

--- Final.r
+++ Final.r
@@ -18,15 +18,21 @@ plot(as.Date(data$ï..Date), data$error1,xlab = "Dates", ylab = "Residuals")
 #HAC Estimators First Regression
 vcovmat <- vcovHAC(firstreg)
 vcovmat
-beta1p <- firstregsum$coefficients[1,1]/vcovmat[1,1]
+beta1t <- firstregsum$coefficients[1,1]/vcovmat[1,1]
+beta1t
+beta1p <- 1 - pnorm(beta1t)
 beta1p
 beta1SE <- sqrt(vcovmat[1,1])
 beta1SE
-beta2p <- firstregsum$coefficients[2,1]/vcovmat[2,2]
+beta2t <- firstregsum$coefficients[2,1]/vcovmat[2,2]
+beta2t
+beta2p <- pnorm(beta2t)
 beta2p
 beta2SE <- sqrt(vcovmat[2,2])
 beta2SE
-beta3p <- firstregsum$coefficients[3,1]/vcovmat[3,3]
+beta3t <- firstregsum$coefficients[3,1]/vcovmat[3,3]
+beta3t
+beta3p <- 1 - pnorm(beta3t)
 beta3p
 beta3SE <- sqrt(vcovmat[3,3])
 beta3SE
@@ -38,23 +44,32 @@ secondregsum
 #HAC estimators second regression
 vcovmat2 <- vcovHAC(secondreg)
 vcovmat2
-beta1p2 <- secondregsum$coefficients[1,1]/vcovmat2[1,1]
-beta1p2
+beta1t2 <- secondregsum$coefficients[1,1]/vcovmat2[1,1]
+beta1t2
+beta1p2 <- 1 - pnorm(beta1t2)
 beta1SE2 <- sqrt(vcovmat2[1,1])
 beta1SE2
-beta2p2 <- secondregsum$coefficients[2,1]/vcovmat2[2,2]
+beta2t2 <- secondregsum$coefficients[2,1]/vcovmat2[2,2]
+beta2t2
+beta2p2 <- pnorm(beta2t2)
 beta2p2
 beta2SE2 <- sqrt(vcovmat2[2,2])
 beta2SE2
-beta3p2 <- secondregsum$coefficients[3,1]/vcovmat2[3,3]
+beta3t2 <- secondregsum$coefficients[3,1]/vcovmat2[3,3]
+beta3t2
+beta3p2 <- 1 - pnorm(beta3t2)
 beta3p2
 beta3SE2 <- sqrt(vcovmat2[3,3])
 beta3SE2
-beta4p2 <- secondregsum$coefficients[4,1]/vcovmat2[4,4]
+beta4t2 <- secondregsum$coefficients[4,1]/vcovmat2[4,4]
+beta4t2
+beta4p2 <- 1 - pnorm(beta4t2)
 beta4p2
 beta4SE2 <- sqrt(vcovmat2[4,4])
 beta4SE2
-beta5p2 <- secondregsum$coefficients[5,1]/vcovmat2[5,5]
+beta5t2 <- secondregsum$coefficients[5,1]/vcovmat2[5,5]
+beta5t2
+beta5p2 <- 1 - pnorm(beta5t2)
 beta5p2
 beta5SE2 <- sqrt(vcovmat2[5,5])
 beta5SE2
